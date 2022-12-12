fa7ff9e8aaef78826948fccf62c73ed38303c684
Jeremy Kropa <93391845+Jerkrop@users.noreply.github.com>
2022-12-12 11:16:23 -0500

    deleted extra page made changes to the html & css along with more functionality to patient page still need to work on admin report & payment more

--- Patient.php
+++ Patient.php
@@ -5,7 +5,7 @@
 
 <h1>Patient Search<h1>
 <div class="flexbox-container1">
-<form action="output_patient.php" method="POST">
+<form action="Patient.php" method="POST">
     
 <div class="input-group mb-3">
  <input type="text" name="search" required value="<?php if(isset($_POST['search'])){echo $_POST['search']; } ?>" class="form-control" placeholder="Search data">
@@ -14,11 +14,7 @@
 </form>
 </form>
 </div>
-<<<<<<< HEAD
 </html>
 <?php
 require "output_patient.php"
 ?>
-=======
-</html>
->>>>>>> 095de30d75de6becd600beb9ef3fe0a3b382335e
